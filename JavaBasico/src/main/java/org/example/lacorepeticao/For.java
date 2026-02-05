541b33efd8c1e12b1ca7dbc8ae6a924745e31950
BrunnoRosa <bruno.r.silva7@ba.estudante.senai.br>
2026-02-04 21:28:47 -0300

    Atividade Java Básico
    Aulas 1 e 2 de DS

---
+++ JavaBasico/src/main/java/org/example/lacorepeticao/For.java
@@ -0,0 +1,13 @@
+package org.example.lacorepeticao;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 100; i <= 120; i++) {
+            if (i % 2 == 0)
+                System.out.println("Números pares:" + i);
+        }
+    }
+}
+
+
