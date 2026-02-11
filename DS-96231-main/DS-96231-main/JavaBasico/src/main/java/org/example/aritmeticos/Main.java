4cba54a49de57db603f2c349de6a8f873a18d817
BrunnoRosa <bruno.r.silva7@ba.estudante.senai.br>
2026-02-10 21:45:10 -0300

    Add files via upload
    Aula 3

---
+++ DS-96231-main/DS-96231-main/JavaBasico/src/main/java/org/example/aritmeticos/Main.java
@@ -0,0 +1,23 @@
+package org.example.aritmeticos;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int a = 10;
+        int b = 2;
+
+        // Operadores aritméticos
+        int multiplicador = a * b;
+        int dividir  = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+
+        // Exibindo resultados
+
+        System.out.println("Miltiplicador: " + multiplicador);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+
+    }
+}
